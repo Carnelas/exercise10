38191b89904079c6fa39e6907fd708a412ccff0e
Carnelas <carnelas@gmail.com>
2018-11-14 19:33:35 +0100

    Probando braker en local

--- message/src/controllers/sendMessage.js
+++ message/src/controllers/sendMessage.js
@@ -1,19 +1,27 @@
 const http = require("http");
 const saveMessage = require("../clients/saveMessage");
-const rollBackQueue = require("./rollBackQueue")
+const rollBackQueue = require("./rollBackQueue");
+const braker = require("../braker");
+const util = require("util");
 
-const random = n => Math.floor(Math.random() * Math.floor(n));
+braker.on("snapshot", snapshot => {
+  console.log(`Circuit open --> ${util.inspect(snapshot.open)}`);
+});
+braker.on("failure", () => console.log("FAIL"));
+braker.on("sucess", () => console.log("SUCESS"));
 
-module.exports = function (message, done) {
-  console.log(message)
+module.exports = function(message, done) {
+  console.log(message);
   const body = JSON.stringify(message);
-  const idQuery = message.qId
-if (message.payment === true){
+  const idQuery = message.qId;
 
+  if (message.payment === true){
 
   const postOptions = {
-    host: "messageapp",
+    host: "localhost",
     port: 3000,
+   /*  host: "messageapp",
+    port: 3000, */
     path: "/message",
     method: "post",
     json: true,
@@ -22,68 +30,85 @@ if (message.payment === true){
       "Content-Length": Buffer.byteLength(body)
     }
   };
+  function asyncFunction(postOptions) {
+    return new Promise(function(resolve, reject) {
+      let postReq = http.request(postOptions, function(response) {
+        if (postRes.statusCode === 200) {
+          saveMessage(
+            {
+              ...message,
+              qId: message.qId,
+              status: "OK"
+            },
+            function(_result, error) {
+              if (error) {
+                console.log("Error 500", error);
+              } else {
+                console.log("Successfull");
+              }
+              idQuery;
+            }
+          );
+          resolve(message);
+        } else if (response.statusCode >= 500) {
+          console.error("Error while sending message");
+        } else {
+          saveMessage(
+            {
+              ...message,
+              qId: message.qId,
+              status: "ERROR",
+              payment: false
+            },
+            () => {
+              rollBackQueue();
+              console.log("Internal server error: SERVICE ERROR");
+            },
+            idQuery
+          );
+        }
+      });
 
-  let postReq = http.request(postOptions);
-
-  postReq.on("response", postRes => {
-    if (postRes.statusCode === 200) {
-      saveMessage({
-          ...message,
-          qId: message.qId,
-          status: "OK"
-        },
-        () => {
-          console.log("Error in server response");
-        }, idQuery
-      );
-    } else {
-      console.error("Error while sending message");
-
-      saveMessage({
-          ...message,
-          qId: message.qId,
-          status: "ERROR",
-          payment: false
-        },
-        () => {
-          rollBackQueue()
-          console.log("Internal server error: SERVICE ERROR");
-        }, idQuery
-      );
-    }
-  });
-
-  postReq.setTimeout(random(1000));
-
-  postReq.on("timeout", () => {
-    console.error("Timeout Exceeded!");
-    postReq.abort();
-
+      postReq.setTimeout(1000);
+      postReq.on("timeout", () => {
+        console.error("Timeout Exceeded!");
+        postReq.abort();
+        saveMessage(
+          {
+            ...message,
+            qId: message.qId,
+            status: "TIMEOUT"
+          },
+          () => {
+            console.log("Internal server error: TIMEOUT");
+          },
+          idQuery
+        );
+        reject(new Error("Timeout error"));
+      });
+      postReq.write(body);
+      postReq.end();
+    });
+  }
+  const circuit = braker.slaveCircuit(asyncFunction);
+  circuit
+    .exec(postOptions)
+    .then(result => {
+      console.log(`result: ${result}`);
+    })
+    .catch(err => {
+      console.error(`${err}`);
+    })
+  } else {
     saveMessage({
-        ...message,
-        qId: message.qId,
-        status: "TIMEOUT"
-      },
-      () => {
-
-        console.log("Internal server error: TIMEOUT");
-      }, idQuery
-    );
-  });
-  postReq.on("error", () => {});
-
-  postReq.write(body);
-  postReq.end();
-} else {
-  saveMessage({
-    ...message,
-    qId: message.qId,
-    status: "INSUFICIENT CREDIT"
-  },
-  () => {
-
-    console.log("Internal server error:INSUFICIENT CREDIT");
-  }, idQuery
-);
-}
-}
+      ...message,
+      qId: message.qId,
+      status: "INSUFICIENT CREDIT"
+    },
+    () => {
+  
+      console.log("Internal server error:INSUFICIENT CREDIT");
+    }, idQuery
+  );
+  }
+};
--- message/src/database.js
+++ message/src/database.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const servers = {
-  primary: "mongodb_message:27027",
-  replica: "replica_message:27028"
+  // primary: "mongodb:27027",
+  // replica: "replica:27028"
+  primary: "127.0.0.1:27027",
+  replica: "127.0.0.1:27028"
 };
 const database = "cabify_bootcamp";
 
